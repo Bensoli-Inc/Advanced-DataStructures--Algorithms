faa831bc688ca41f29606cb1e52627d3310b7d7a
Bensoli-Inc <benswambua96@gmail.com>
2024-09-04 14:33:24 +0300

    linked list insertAt function

--- src/LinkedList/Linkedlist.java
+++ src/LinkedList/Linkedlist.java
@@ -34,6 +34,26 @@ public class Linkedlist
         head =node;
     }
 
+    public void insertAt(int index, int data)
+    {
+        Node node = new Node();
+        node.data = data;
+        node.next = null;
+
+        if(index==0)
+        {
+            insertAtStart(data);
+        }
+        else {
+            Node n = head;
+            for (int i = 0; i <= index - 1; i++) {
+                n = n.next;
+            }
+            node.next = n.next;
+            n.next = node;
+        }
+    }
+
     public void show()
     {
         Node node = head;
--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -9,6 +9,7 @@ public class Main
         list.insert(45);
         list.insert(12);
         list.insertAtStart(25);
+        list.insertAt(0, 55);
 
         list.show();
     }
