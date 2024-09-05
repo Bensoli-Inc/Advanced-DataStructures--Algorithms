c04fdbe26578453b687dd527ac2103615ddc8725
Bensoli-Inc <benswambua96@gmail.com>
2024-09-05 06:24:08 +0300

    creating a dynamic expandable array (stack)

---
+++ src/DynamicStack/Main.java
@@ -0,0 +1,21 @@
+package DynamicStack;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Dstack nums = new Dstack();
+        nums.push(15);
+        nums.show();
+        nums.push(10);
+        nums.show();
+        nums.push(22);
+        nums.show();
+        nums.push(25);
+        nums.show();
+        nums.push(30);
+        nums.show();
+        nums.push(55);
+
+    }
+}
