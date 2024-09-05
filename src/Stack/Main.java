ac0ce00f1b8feaa470f76d774b4dc0ab9a7f771d
Bensoli-Inc <benswambua96@gmail.com>
2024-09-05 05:16:43 +0300

    introduction to stack

---
+++ src/Stack/Main.java
@@ -0,0 +1,15 @@
+package Stack;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Stack nums = new Stack();
+        nums.push(15);
+        nums.push(20);
+
+        nums.pop();
+        nums.peek();
+    }
+}
+
---
+++ src/Stack/Stack.java
@@ -0,0 +1,19 @@
+package Stack;
+
+public class Stack
+{
+    public void push(data)
+    {
+
+    }
+
+    public void pop(data)
+    {
+
+    }
+
+    public void peek(data)
+    {
+
+    }
+}
